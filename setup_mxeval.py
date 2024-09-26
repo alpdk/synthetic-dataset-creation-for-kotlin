69dd586a7ecb372e116428c4fb61e0c720dadca5
alpdk <alexander@podkopaev.net>
2024-09-26 19:17:04 +0300

    Created method setup_mxeval method.

---
+++ setup_mxeval.py
@@ -0,0 +1,18 @@
+import os
+import sys
+
+def setup_mxeval(mxeval_parent_dir):
+    # Get the path to the mxeval directory
+    mxeval_dir = os.path.join(mxeval_parent_dir, 'mxeval')
+
+    # Add mxeval to sys.path
+    if sys.path[0] != mxeval_dir:
+
+        if mxeval_dir in sys.path:
+            sys.path.remove(mxeval_dir)
+
+        sys.path.insert(0, mxeval_dir)
+
+    # Print all system path for packages
+    # sys.path.remove(mxeval_dir)
+    # print(sys.path, sep='\n')
